36b493c15f9ebc97b880a8a1a08dba5e9947fdeb
gyro永不抽风 <1247006353@qq.com>
2021-09-25 20:54:13 +0800

    feat: add settings

--- YuzuMarker/YuzuMarker.Properties/Settings.cs
+++ YuzuMarker/YuzuMarker.Properties/Settings.cs
@@ -2,9 +2,17 @@
 
 namespace YuzuMarker.Properties
 {
+    public enum PSBridgeType: uint
+    {
+        COM = 0,
+        Extension = 1
+    }
+
     public static class Settings
     {
         // TODO: when wpf starts up, load settings
+        public static int PhotoshopExtensionHTTPServerPort = 4016;
 
+        public static PSBridgeType PhotoshopBridgeType = PSBridgeType.COM;
     }
 }
