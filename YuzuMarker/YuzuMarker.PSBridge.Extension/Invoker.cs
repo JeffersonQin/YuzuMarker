c679e2c10795d8c2fd10e1fb8bc97772d299b0ae
gyro永不抽风 <1247006353@qq.com>
2021-09-26 23:08:26 +0800

    feat: update exception handling for refine api interface for ps extension bridge

---
+++ YuzuMarker/YuzuMarker.PSBridge.Extension/Invoker.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YuzuMarker.PSBridge.Extension
+{
+    public static class Invoker
+    {
+        public static bool ExistLayerSet(string layerSetName)
+        {
+            JObject ret = WebUtil.GET("existLayerSet", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName }
+            });
+            try
+            {
+                return (bool)ret["exist"];
+            }
+            catch
+            {
+                throw new Exception("YuzuMarker.PSBridge.Extension.Invoker: failed to read data");
+            }
+        }
+
+        public static bool ExistArtLayer(string layerSetName, string artLayerName)
+        {
+            JObject ret = WebUtil.GET("existArtLayer", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName },
+                { "artLayerName", artLayerName }
+            });
+            try
+            {
+                return (bool)ret["exist"];
+            }
+            catch
+            {
+                throw new Exception("YuzuMarker.PSBridge.Extension.Invoker: failed to read data");
+            }
+        }
+
+        public static void OpenFile(string path)
+        {
+            WebUtil.GET("openFile", new Dictionary<string, string>
+            {
+                { "path", path }
+            });
+        }
+
+        public static void CreateFile(string path)
+        {
+            WebUtil.GET("createFile", new Dictionary<string, string>
+            {
+                { "path", path }
+            });
+        }
+
+        public static void AddLayerSet(string layerSetName)
+        {
+            WebUtil.GET("addLayerSet", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName }
+            });
+        }
+
+        public static void AddArtLayer(string layerSetName, string artLayerName)
+        {
+            WebUtil.GET("addArtLayer", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName },
+                { "artLayerName", artLayerName }
+            });
+        }
+
+        public static void RemoveLayerSet(string layerSetName)
+        {
+            WebUtil.GET("removeLayerSet", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName }
+            });
+        }
+
+        public static void RemoveArtLayer(string layerSetName, string artLayerName)
+        {
+            WebUtil.GET("removeArtLayer", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName },
+                { "artLayerName", artLayerName }
+            });
+        }
+
+        public static void AddTextLayer(string layerSetName, string artLayerName)
+        {
+            WebUtil.GET("addTextLayer", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName },
+                { "artLayerName", artLayerName }
+            });
+        }
+
+        public static void SetTextLayer(string layerSetName, string artLayerName)
+        {
+            WebUtil.GET("setTextLayer", new Dictionary<string, string>
+            {
+                { "layerSetName", layerSetName },
+                { "artLayerName", artLayerName }
+            });
+        }
+    }
+}
---
+++ YuzuMarker/YuzuMarker.PSBridge.Extension/WebUtil.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace YuzuMarker.PSBridge.Extension
+{
+    public static class WebUtil
+    {
+        private static WebClient webClient = new WebClient();
+
+        public static JObject GET(string requestUri, Dictionary<string, string> param)
+        {
+            webClient.QueryString.Clear();
+            foreach (KeyValuePair<string, string> kv in param)
+            {
+                webClient.QueryString.Add(kv.Key, kv.Value);
+            }
+            
+            string response;
+            try
+            {
+                response = webClient.DownloadString("http://localhost:" +
+                    Properties.Settings.PhotoshopExtensionHTTPServerPort + "/" + requestUri);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("YuzuMarker.PSBridge.Extension.WebUtil: communication with photoshop failed, check whether the extension was enabled. \nMessage: " + e.Message);
+            }
+            
+            JObject obj, data;
+            string status, error;
+            try
+            {
+                obj = JObject.Parse(response);
+                status = (string)obj["status"];
+                error = (string)obj["error"];
+                data = (JObject)obj["data"];
+            }
+            catch (Exception e)
+            {
+                throw new Exception("YuzuMarker.PSBridge.Extension.WebUtil: JSON Parsing failed. \nMessage: " + e.Message);
+            }
+
+            if (error == null || status == null)
+            {
+                throw new Exception("YuzuMarker.PSBridge.Extension.WebUtil: status and error received null.");
+            }
+
+            if (status != "success")
+            {
+                throw new Exception("YuzuMarker.PSBridge.Extension.WebUtil: operation failed, error: " + error);
+            }
+
+            return data;
+        }
+    }
+}
