150eb9d1e8e204377b821e4a5e9af1c634d0c3d8
gyro永不抽风 <1247006353@qq.com>
2021-09-30 16:42:55 +0800

    feat: add global instance manager

---
+++ YuzuMarker/YuzuMarker/Manager/YuzuMarkerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YuzuMarker.Files;
+
+namespace YuzuMarker.Manager
+{
+    public static class YuzuMarkerManager
+    {
+        private static YuzuProject _Project = null;
+
+        public static YuzuProject Project {
+            get
+            {
+                return _Project;
+            }
+            set
+            {
+                _Project = value;
+            }
+        }
+
+        private static YuzuImage _Image = null;
+
+        public static YuzuImage Image
+        {
+            get
+            {
+                return _Image;
+            }
+            set
+            {
+                _Image = value;
+            }
+        }
+    }
+}
