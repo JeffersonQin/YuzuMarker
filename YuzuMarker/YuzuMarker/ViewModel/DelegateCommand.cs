e4f662720f0ca7da3de7628f4407697b0c131911
gyro永不抽风 <1247006353@qq.com>
2021-09-30 09:05:35 +0800

    feat: add base classes for mvvm

---
+++ YuzuMarker/YuzuMarker/ViewModel/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace YuzuMarker.ViewModel
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        public Action<T> CommandAction { get; set; }
+
+        public Func<T, bool> CanExecuteFunc { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            CommandAction((T)parameter);
+        }
+    }
+}
---
+++ YuzuMarker/YuzuMarker/ViewModel/NotifyObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace YuzuMarker.ViewModel
+{
+    public class NotifyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
