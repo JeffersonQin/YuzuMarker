4172ce6a9da9b1001b7482f18d9d632832a322bb
gyro永不抽风 <1247006353@qq.com>
2021-09-30 16:43:08 +0800

    feat: add exception handler

---
+++ YuzuMarker/YuzuMarker/Utils/ExceptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace YuzuMarker.Utils
+{
+    public static class ExceptionHandler
+    {
+        public static void ShowExceptionMessage(Exception e)
+        {
+            MessageBox.Show("发生错误：" + e.Message + "\n堆栈信息：\n" + e.StackTrace);
+        }
+    }
+}
