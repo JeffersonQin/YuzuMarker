d710d43196ab8b4d991c2412fb896b802619cfd3
gyro永不抽风 <1247006353@qq.com>
2021-09-30 16:43:47 +0800

    feat: update delegate command for cases without parameter

--- YuzuMarker/YuzuMarker/ViewModel/DelegateCommand.cs
+++ YuzuMarker/YuzuMarker/ViewModel/DelegateCommand.cs
@@ -5,6 +5,35 @@ using System.Windows.Input;
 
 namespace YuzuMarker.ViewModel
 {
+    public class DelegateCommand : ICommand
+    {
+        public Action CommandAction { get; set; }
+
+        public Func<bool> CanExecuteFunc { get; set; }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteFunc == null || CanExecuteFunc();
+        }
+
+        public void Execute(object parameter)
+        {
+            CommandAction();
+        }
+    }
+
     public class DelegateCommand<T> : ICommand
     {
         public Action<T> CommandAction { get; set; }
