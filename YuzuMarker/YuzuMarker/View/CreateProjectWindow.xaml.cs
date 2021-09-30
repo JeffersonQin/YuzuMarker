fbf4a0bf648ce31c90ca519e0c3eacc6bb6e7061
gyro永不抽风 <1247006353@qq.com>
2021-09-30 16:44:33 +0800

    feat: add mvvm-style create project window using event to pass parameters

---
+++ YuzuMarker/YuzuMarker/View/CreateProjectWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using YuzuMarker.ViewModel;
+
+namespace YuzuMarker.View
+{
+    /// <summary>
+    /// CreateProjectWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class CreateProjectWindow : Window
+    {
+        public delegate void CreateProjectEventHandler(object sender, string fileName, string projectName, string path);
+
+        public event CreateProjectEventHandler CreateProjectEvent;
+
+        private bool ButtonClicked = false;
+
+        public CreateProjectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            YuzuCreateProjectViewModel vm = DataContext as YuzuCreateProjectViewModel;
+            CreateProjectEvent(this, vm.FileName, vm.ProjectName, vm.Path);
+            ButtonClicked = true;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (!ButtonClicked)
+                CreateProjectEvent(this, "", "", "");
+        }
+    }
+}
---
+++ YuzuMarker/YuzuMarker/ViewModel/YuzuCreateProjectViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YuzuMarker.ViewModel
+{
+    public class YuzuCreateProjectViewModel : NotifyObject
+    {
+        private string fileName;
+
+        public string FileName
+        {
+            get
+            {
+                return fileName;
+            }
+            set
+            {
+                fileName = value;
+                RaisePropertyChanged("FileName");
+            }
+        }
+
+        private string projectName;
+
+        public string ProjectName
+        {
+            get
+            {
+                return projectName;
+            }
+            set
+            {
+                projectName = value;
+                RaisePropertyChanged("ProjectName");
+            }
+        }
+
+        private string path;
+
+        public string Path
+        {
+            get
+            {
+                return path;
+            }
+            set
+            {
+                path = value;
+                RaisePropertyChanged("Path");
+            }
+        }
+
+        private DelegateCommand chooseDirectoryCommand;
+
+        public DelegateCommand ChooseDirectoryCommand
+        {
+            get
+            {
+                if (chooseDirectoryCommand == null)
+                    chooseDirectoryCommand = new DelegateCommand
+                    {
+                        CommandAction = () =>
+                        {
+                            try
+                            {
+                                OpenFileDialog openFileDialog = new OpenFileDialog
+                                {
+                                    Multiselect = false,
+                                    Title = "选择新建项目的位置",
+                                    CheckFileExists = false,
+                                    CheckPathExists = true,
+                                    FileName = "Folder Selection"
+                                };
+                                if (openFileDialog.ShowDialog() == true)
+                                {
+                                    Path = System.IO.Path.GetDirectoryName(openFileDialog.FileNames[0]);
+                                    RaisePropertyChanged("Path");
+                                }
+                            } catch (Exception e)
+                            {
+                                Utils.ExceptionHandler.ShowExceptionMessage(e);
+                            }
+                        }
+                    };
+                return chooseDirectoryCommand;
+            }
+        }
+    }
+}
