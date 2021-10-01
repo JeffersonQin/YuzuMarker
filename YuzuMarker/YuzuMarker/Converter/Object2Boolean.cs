e6febdf8da917d5f7f41626a6e77c4c7bb441b4f
gyro永不抽风 <1247006353@qq.com>
2021-10-01 20:40:50 +0800

    feat: add type converter works for binding

---
+++ YuzuMarker/YuzuMarker/Converter/Object2Boolean.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace YuzuMarker.Converter
+{
+    public class Object2Boolean : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
