3c257016cdb2c67a5ae15b8d77a535f884dfbf26
gyro永不抽风 <1247006353@qq.com>
2021-10-08 18:46:06 +0800

    feat: add bool2color converter

---
+++ YuzuMarker/YuzuMarker/Converter/Boolean2ColorBrush.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace YuzuMarker.Converter
+{
+    public class Boolean2ColorBrush : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (((bool) value) == true)
+            {
+                return new SolidColorBrush(Colors.MediumSpringGreen);
+            }
+            return new SolidColorBrush(Colors.LightCoral);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
