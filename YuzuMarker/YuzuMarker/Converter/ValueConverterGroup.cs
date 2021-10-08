8df806a072f968f4ae0e33f6f3edc6d3173b4c4f
gyro永不抽风 <1247006353@qq.com>
2021-10-08 21:14:08 +0800

    feat: add value converter group

---
+++ YuzuMarker/YuzuMarker/Converter/ValueConverterGroup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace YuzuMarker.Converter
+{
+    /// <summary>
+    /// From: https://stackoverflow.com/questions/2607490/is-there-a-way-to-chain-multiple-value-converters-in-xaml
+    /// </summary>
+    public class ValueConverterGroup : List<IValueConverter>, IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return this.Aggregate(value, (current, converter) => converter.Convert(current, targetType, parameter, culture));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
