eea808cfe13ed43ec49152b2b18feb9a91f92d70
gyro永不抽风 <1247006353@qq.com>
2021-10-08 21:13:55 +0800

    feat: add listview item to index converter

---
+++ YuzuMarker/YuzuMarker/Converter/IndexConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace YuzuMarker.Converter
+{
+    /// <summary>
+    /// From: https://stackoverflow.com/questions/660528/how-to-display-row-numbers-in-a-listview
+    /// </summary>
+    public class IndexConverter : IValueConverter
+    {
+        public object Convert(object value, Type TargetType, object parameter, CultureInfo culture)
+        {
+            ListViewItem item = (ListViewItem)value;
+            ListView listView = ItemsControl.ItemsControlFromItemContainer(item) as ListView;
+            int index = listView.ItemContainerGenerator.IndexFromContainer(item);
+            return index;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
