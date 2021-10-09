4112f595cbc3dcd3228a55d5940617ed0312bbf8
gyro永不抽风 <1247006353@qq.com>
2021-10-09 10:57:41 +0800

    feat: add notation group to index converter

---
+++ YuzuMarker/YuzuMarker/Converter/NotationGroup2Index.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using YuzuMarker.DataFormat;
+
+namespace YuzuMarker.Converter
+{
+    public class NotationGroup2Index : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Manager.YuzuMarkerManager.Image.NotationGroups.IndexOf((YuzuNotationGroup)value) + 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (((int)value) - 1 < Manager.YuzuMarkerManager.Image.NotationGroups.Count)
+                return Manager.YuzuMarkerManager.Image.NotationGroups[((int)value) - 1];
+            return null;
+        }
+    }
+}
