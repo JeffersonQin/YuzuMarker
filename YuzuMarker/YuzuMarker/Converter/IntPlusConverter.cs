096395ca323640b0c58b59bfa9f961fc5ac513e7
gyro永不抽风 <1247006353@qq.com>
2021-10-10 19:19:07 +0800

    feat: update plus 1 converter to more general converter

---
+++ YuzuMarker/YuzuMarker/Converter/IntPlusConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace YuzuMarker.Converter
+{
+    public class IntPlusConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return 0;
+            if (parameter == null) parameter = 0;
+
+            if (int.TryParse(value.ToString(), out int number) && int.TryParse(parameter.ToString(), out int param))
+                return number + param;
+            return 0;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return 0;
+            if (parameter == null) parameter = 0;
+
+            if (int.TryParse(value.ToString(), out int number) && int.TryParse(parameter.ToString(), out int param))
+                return number - param;
+            return 0;
+        }
+    }
+}
