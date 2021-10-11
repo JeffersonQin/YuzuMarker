dfea687bba93886f8bb81f0fdbbccb6c8ea85cec
gyro永不抽风 <1247006353@qq.com>
2021-10-11 19:23:32 +0800

    feat: implement conversion between notationgroup and canvasitem

--- YuzuMarker/YuzuMarker/Converter/NotationGroup2CanvasItem.cs
+++ YuzuMarker/YuzuMarker/Converter/NotationGroup2CanvasItem.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Globalization;
 using System.Text;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using YuzuMarker.DataFormat;
+using YuzuMarker.Utils;
 
 namespace YuzuMarker.Converter
 {
@@ -10,7 +16,23 @@ namespace YuzuMarker.Converter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            Grid container =  new Grid();
+
+            YuzuNotationGroup notationGroup = (YuzuNotationGroup)value;
+
+            if (notationGroup.CleaningNotation.CleaningNotationType != YuzuCleaningNotationType.None)
+            {
+                Polygon cleaningPolygon = new Polygon();
+                cleaningPolygon.Points = notationGroup.CleaningNotation.CleaningPoints.ToPointCollection();
+                if (notationGroup.CleaningNotation.CleaningNotationType == YuzuCleaningNotationType.Custom)
+                    cleaningPolygon.Stroke = new SolidColorBrush(Colors.Red);
+                else
+                    cleaningPolygon.Stroke = new SolidColorBrush(Colors.Blue);
+
+                container.Children.Add(cleaningPolygon);
+            }
+
+            return container;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
