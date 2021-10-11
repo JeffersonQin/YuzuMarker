f9655aa3db5941d3c8c973f3d669a4668b241404
gyro永不抽风 <1247006353@qq.com>
2021-10-11 20:42:11 +0800

    feat: add color extension

---
+++ YuzuMarker/YuzuMarker/Utils/ColorExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace YuzuMarker.Utils
+{
+    public static class ColorExtension
+    {
+        public static System.Windows.Media.Color ToColor(this uint color)
+        {
+            return System.Windows.Media.Color.FromArgb(
+                (byte) ((color & 0xFF000000) >> 24),
+                (byte) ((color & 0x00FF0000) >> 16),
+                (byte) ((color & 0x0000FF00) >> 8),
+                (byte) (color & 0x000000FF)
+            );
+        }
+    }
+}
