2d229f94cf8d411cd626edb1d674490b18e5e584
gyro永不抽风 <1247006353@qq.com>
2021-10-11 19:26:22 +0800

    feat: add conversion extension for Pointcollection and List<PointF>

---
+++ YuzuMarker/YuzuMarker/Utils/PointsCollectionExtension.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Media;
+
+namespace YuzuMarker.Utils
+{
+    public static class PointsCollectionExtension
+    {
+        public static List<PointF> ToGenericPoints(this PointCollection pointCollection)
+        {
+            List<PointF> points = new List<PointF>();
+            foreach (var p in pointCollection)
+                points.Add(new PointF((float)p.X, (float)p.Y));
+            return points;
+        }
+
+        public static PointCollection ToPointCollection(this List<PointF> points)
+        {
+            PointCollection pointCollection = new PointCollection();
+            points.ForEach(p => pointCollection.Add(new System.Windows.Point(p.X, p.Y)));
+            return pointCollection;
+        }
+    }
+}
