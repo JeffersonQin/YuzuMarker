299b3a44ac10e1a2c2c60a6991bad9eb7930e74e
gyro永不抽风 <1247006353@qq.com>
2021-10-12 22:07:26 +0800

    feat: add converter: NotationGroup2CanvasItem

---
+++ YuzuMarker/YuzuMarker/Converter/NotationGroup2Boolean4Cleaning.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using YuzuMarker.DataFormat;
+
+namespace YuzuMarker.Converter
+{
+    public class NotationGroup2Boolean4NormalCleaning : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            if (((YuzuNotationGroup)value).CleaningNotation.CleaningNotationType == YuzuCleaningNotationType.Normal)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    public class NotationGroup2Boolean4CustomCleaning : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return false;
+            if (((YuzuNotationGroup)value).CleaningNotation.CleaningNotationType == YuzuCleaningNotationType.Custom)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
