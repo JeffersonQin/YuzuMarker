5324e8b672707cfc1f9dd9a977d31066f51e641a
gyro永不抽风 <1247006353@qq.com>
2021-10-12 22:09:05 +0800

    feat: add converter: Cleaning2StatusString

---
+++ YuzuMarker/YuzuMarker/Converter/Cleaning2StatusString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using YuzuMarker.DataFormat;
+
+namespace YuzuMarker.Converter
+{
+    public class Cleaning2StatusString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            YuzuCleaningNotation cleaningNotation = (YuzuCleaningNotation) value;
+            if (cleaningNotation.CleaningNotationType == YuzuCleaningNotationType.Custom) return "PS自定义蒙版";
+            if (cleaningNotation.CleaningPoints.Count == 0) return "不涂白";
+            return "区域已选择";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
