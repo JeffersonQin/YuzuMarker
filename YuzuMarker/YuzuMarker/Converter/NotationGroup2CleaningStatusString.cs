49573fa95570877c88ab5b142ba6f8cd1950a48b
gyro永不抽风 <1247006353@qq.com>
2021-10-12 22:08:12 +0800

    feat: add converter: NotationGroup2CleaningStatusString

---
+++ YuzuMarker/YuzuMarker/Converter/NotationGroup2CleaningStatusString.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using YuzuMarker.DataFormat;
+using YuzuMarker.Properties;
+using YuzuMarker.Utils;
+
+namespace YuzuMarker.Converter
+{
+    public class NotationGroup2CleaningStatusString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            YuzuNotationGroup notationGroup = (YuzuNotationGroup)value;
+            if (notationGroup == null) return null;
+
+            if (notationGroup.CleaningNotation.CleaningPoints.Count == 0) return " [ 未选区域 ] ";
+            return " [ 已选区域 ] ";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
