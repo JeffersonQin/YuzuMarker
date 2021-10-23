a9272b664f78a5f3d530719eb71d52a77536c450
gyro永不抽风 <1247006353@qq.com>
2021-10-23 21:14:17 +0800

    feat: update build script, use venv

--- YuzuMarker/YuzuMarker.IPS/Build.bat
+++ YuzuMarker/YuzuMarker.IPS/Build.bat
@@ -1,1 +1,19 @@
-pyinstaller.exe -D -y --distpath %1 YuzuIPS.py
+chcp 65001
+
+:: 判断 venv 是否存在, 不存在则建立
+:: 需要将 python 修改为目标解释器 python.exe 的路径
+if not exist venv (
+	python -m venv venv
+)
+
+:: 进入 venv
+call venv\Scripts\activate.bat
+
+:: 升级 pip
+python -m pip install --upgrade pip
+
+:: 安装依赖
+pip install -r requirements.txt --index-url https://mirrors.aliyun.com/pypi/simple
+
+:: pyinstaller 打包
+.\venv\Scripts\pyinstaller.exe -D -y --distpath %1 YuzuIPS.py
