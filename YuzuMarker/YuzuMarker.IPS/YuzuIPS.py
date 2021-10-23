02c623b241e4136ac36a014ea0632b754b45f951
gyro永不抽风 <1247006353@qq.com>
2021-10-23 21:14:58 +0800

    feat: add a basic server based on fastapi

--- YuzuMarker/YuzuMarker.IPS/YuzuIPS.py
+++ YuzuMarker/YuzuMarker.IPS/YuzuIPS.py
@@ -1,4 +1,11 @@
-import os
+from fastapi import FastAPI
+import uvicorn
 
-print("test")
-input("...")
+host = "127.0.0.1"
+port = 1029
+
+app = FastAPI()
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host=host, port=port)
