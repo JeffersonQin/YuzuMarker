88213e24c7ce9ba099e4d9018b4e7b330918b391
gyro永不抽风 <1247006353@qq.com>
2021-10-25 14:39:44 +0800

    feat: move some classes to common project

--- YuzuMarker/YuzuMarker.Common/IOUtils.cs
+++ YuzuMarker/YuzuMarker.Common/IOUtils.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace YuzuMarker.DataFormat
+namespace YuzuMarker.Common
 {
     public static class IOUtils
     {
---
+++ YuzuMarker/YuzuMarker.Common/NotifyObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace YuzuMarker.Common
+{
+    public class NotifyObject : INotifyPropertyChanged
+    {
+        public bool SetProperty<T>(ref T backingStore, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+                return false;
+
+            backingStore = value;
+            onChanged?.Invoke();
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
