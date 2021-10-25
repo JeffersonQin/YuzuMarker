5e7157a1719cd65af97534a88738db0965beb697
gyro永不抽风 <1247006353@qq.com>
2021-10-25 14:41:32 +0800

    feat: adapt to new data format

--- YuzuMarker/YuzuMarker.Git/Cryption.cs
+++ YuzuMarker/YuzuMarker.Git/Cryption.cs
@@ -9,13 +9,12 @@ namespace YuzuMarker.Git
 {
     public static class Cryption
     {
-        public static void SetProjectEncryptStatus<LP, LI>(YuzuProject<LP, LI> project, string password)
-            where LP : IList<YuzuImage<LI>>, new() where LI : IList<YuzuNotationGroup>, new()
+        public static void SetProjectEncryptStatus(YuzuProject project, string password)
         {
-            string path = project.path;
+            string path = project.Path;
             // encrypt: password is not "" or null
             JObject yuzuGitSettings = new JObject();
-            if (password != "" && password != null)
+            if (!string.IsNullOrEmpty(password))
             {
                 yuzuGitSettings.Add("encryption", true);
                 File.WriteAllText(Path.Combine(path, "./.gitignore"), "Images/\nPSD/\n.password");
@@ -35,26 +34,23 @@ namespace YuzuMarker.Git
             File.WriteAllText(Path.Combine(path, "./.yuzugitsettings"), yuzuGitSettings.ToString());
         }
 
-        public static bool GetProjectEncryptStatus<LP, LI>(YuzuProject<LP, LI> project)
-            where LP : IList<YuzuImage<LI>>, new() where LI : IList<YuzuNotationGroup>, new()
+        public static bool GetProjectEncryptStatus(YuzuProject project)
         {
-            string path = project.path;
+            string path = project.Path;
             if (!File.Exists(Path.Combine(path, "./.yuzugitsettings"))) return false;
             JObject yuzuSettings = JObject.Parse(File.ReadAllText(Path.Combine(path, "./.yuzugitsettings")));
             return (bool)yuzuSettings["encryption"];
         }
 
-        public static void EncryptProject<LP, LI>(YuzuProject<LP, LI> project)
-            where LP : IList<YuzuImage<LI>>, new() where LI : IList<YuzuNotationGroup>, new()
+        public static void EncryptProject(YuzuProject project)
         {
-            string path = project.path;
+            string path = project.Path;
             if (!File.Exists(Path.Combine(path, "./.password")))
                 throw new Exception("YuzuMarker.Git Error: .password file does not exist. Please configure correctly.");
             // TODO
         }
 
-        public static void DecryptProject<LP, LI>(YuzuProject<LP, LI> project)
-            where LP : IList<YuzuImage<LI>>, new() where LI : IList<YuzuNotationGroup>, new()
+        public static void DecryptProject(YuzuProject project)
         {
             // TODO
         }
