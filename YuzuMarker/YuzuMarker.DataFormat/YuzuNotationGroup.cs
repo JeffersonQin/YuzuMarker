44f542fcf388be1bdc14bfd548cbf34b84e77f48
gyro永不抽风 <1247006353@qq.com>
2021-10-30 17:01:40 +0800

    feat: implement temp cleaning notation with opencvsharp

--- YuzuMarker/YuzuMarker.DataFormat/YuzuNotationGroup.cs
+++ YuzuMarker/YuzuMarker.DataFormat/YuzuNotationGroup.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using OpenCvSharp;
 using YuzuMarker.BasicDataFormat;
 
 namespace YuzuMarker.DataFormat
 {
     public class YuzuNotationGroup : BasicYuzuNotationGroup
     {
+        public ResourcesTracker NotationResourcesTracker;
+        
         public YuzuCleaningNotation CleaningNotation { get; set; }
 
         public YuzuNotationGroup(YuzuImage parentImage, int x, int y, string text, bool finished) : base(parentImage, x, y, text, finished)
@@ -19,9 +23,36 @@ namespace YuzuMarker.DataFormat
             CleaningNotation = new YuzuCleaningNotation(YuzuCleaningNotationType.Normal);
         }
         
-        // TODO: refactor start: 新增 load cleaning notation (1. 尝试从 temp 2. 尝试复制到 temp 3. 记得 Ensure)
-        
-        // TODO: refactor end
+        public void LoadNotationResource()
+        {
+            NotationResourcesTracker = new ResourcesTracker();
+            var tempMaskImagePath = Path.Combine(ParentImage.GetImageTempPath(), "./" + Timestamp + "-cleaning-mask.png");
+            if (!File.Exists(tempMaskImagePath))
+            {
+                var maskImagePath = Path.Combine(ParentImage.GetImageNotationPath(), "./" + Timestamp + "-cleaning-mask.png");
+                if (!File.Exists(maskImagePath))
+                {
+                    using var src = new Mat(ParentImage.GetImageFilePath());
+                    CleaningNotation.CleaningMask = NotationResourcesTracker.NewMat(new Size(src.Cols, src.Rows),
+                        MatType.CV_8UC1, new Scalar(0));
+                }
+                else
+                {
+                    CleaningNotation.CleaningMask = NotationResourcesTracker.T(new Mat(maskImagePath, ImreadModes.Grayscale));
+                }
+            }
+            else
+            {
+                CleaningNotation.CleaningMask = NotationResourcesTracker.T(new Mat(tempMaskImagePath, ImreadModes.Grayscale));
+            }
+        }
+
+        public void UnloadNotationResource()
+        {
+            var tempMaskImagePath = Path.Combine(ParentImage.GetImageTempPath(), "./" + Timestamp + "-cleaning-mask.png");
+            Cv2.ImWrite(tempMaskImagePath, CleaningNotation.CleaningMask);
+            NotationResourcesTracker.Dispose();
+        }
 
         // Other kinds of notations
     }
