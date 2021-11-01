a6b675022604f54c21c660efe191585f3f98f542
gyro永不抽风 <1247006353@qq.com>
2021-11-01 23:46:20 +0800

    feat: add selection type converter

---
+++ YuzuMarker/YuzuMarker/Converter/SelectionTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using YuzuMarker.Model;
+
+namespace YuzuMarker.Converter
+{
+    public class SelectionTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null) return false;
+            if ((int)value == int.Parse(parameter.ToString() ?? "-1")) return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (SelectionType)int.Parse(parameter.ToString());
+        }
+    }
+}
