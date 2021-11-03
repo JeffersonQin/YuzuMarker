bb5fa7b2122130e9a86c9f10c8362aaf31ab153e
gyro永不抽风 <1247006353@qq.com>
2021-11-03 17:26:06 +0800

    feat: expose 'beforeChanged' parameter in NotifyObject

--- YuzuMarker/YuzuMarker.Common/NotifyObject.cs
+++ YuzuMarker/YuzuMarker.Common/NotifyObject.cs
@@ -10,11 +10,12 @@ namespace YuzuMarker.Common
     {
         public bool SetProperty<T>(ref T backingStore, T value,
             [CallerMemberName] string propertyName = "",
+            Action beforeChanged = null,
             Action onChanged = null)
         {
             if (EqualityComparer<T>.Default.Equals(backingStore, value))
                 return false;
-
+            beforeChanged?.Invoke();
             backingStore = value;
             onChanged?.Invoke();
             RaisePropertyChanged(propertyName);
