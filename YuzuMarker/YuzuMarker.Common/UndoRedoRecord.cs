42c6fd7588c42996f4ed9d755b142cc35d6ca0d6
gyro永不抽风 <1247006353@qq.com>
2021-11-03 17:25:24 +0800

    feat: add undo redo record class

---
+++ YuzuMarker/YuzuMarker.Common/UndoRedoRecord.cs
@@ -0,0 +1,32 @@
+﻿namespace YuzuMarker.Common
+{
+    public class UndoRedoRecord
+    {
+        public object Value;
+
+        public delegate void DelegateActionWithValue(object value);
+
+        public delegate object DelegateActionReturnValue();
+
+        public delegate object DelegateActionWithAndReturnValue(object value);
+
+        public DelegateActionWithValue DisposeAction, SetValueAction, UndoAction, RedoAction;
+
+        public DelegateActionReturnValue GetValueAction;
+
+        public UndoRedoRecord(object value, 
+            DelegateActionWithValue setValueAction,
+            DelegateActionReturnValue getValueAction, 
+            DelegateActionWithValue undoAction = null, 
+            DelegateActionWithValue redoAction = null, 
+            DelegateActionWithValue disposeAction = null)
+        {
+            Value = value;
+            SetValueAction = setValueAction;
+            GetValueAction = getValueAction;
+            UndoAction = undoAction;
+            RedoAction = redoAction;
+            DisposeAction = disposeAction;
+        }
+    }
+}
