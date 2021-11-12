e6be643ddcfdf91ef4045a8c8d49900546788eab
gyro永不抽风 <1247006353@qq.com>
2021-11-12 19:56:23 +0800

    feat: add conditional scroll scrollviewer

---
+++ YuzuMarker/YuzuMarker/Control/ConditionalScrollScrollViewer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace YuzuMarker.Control
+{
+    public class ConditionalScrollScrollViewer : ScrollViewer
+    {
+        public delegate bool ShouldScrollDelegate(MouseWheelEventArgs e);
+
+        public ShouldScrollDelegate ShouldScrollEventHandler;
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (ShouldScrollEventHandler(e))
+            {
+                if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+                {
+                    ScrollToHorizontalOffset(HorizontalOffset - e.Delta);
+                    e.Handled = true;
+                }
+                else
+                {
+                    ScrollToVerticalOffset(VerticalOffset - e.Delta);
+                    e.Handled = true;
+                }
+            }
+        }
+    }
+}
