5ffe390785199223ab5f96f25c7ac23f0f77bda0
gyro永不抽风 <1247006353@qq.com>
2021-11-13 18:12:42 +0800

    refactor: pack bit operation

---
+++ YuzuMarker/YuzuMarker/Utils/BitOperation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace YuzuMarker.Utils
+{
+    public static class BitOperation
+    {
+        /// <summary>
+        /// 取指针所在高位数值。
+        /// </summary>
+        public static int HIWORD(IntPtr ptr)
+        {
+            unchecked
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    var val64 = ptr.ToInt64();
+                    return (short) ((val64 >> 16) & 0xFFFF);
+                }
+                var val32 = ptr.ToInt32();
+                return (short) ((val32 >> 16) & 0xFFFF);   
+            }
+        }
+
+        /// <summary>
+        /// 取指针所在低位数值。
+        /// </summary>
+        public static int LOWORD(IntPtr ptr)
+        {
+            unchecked
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    var val64 = ptr.ToInt64();
+                    return (short)(val64 & 0xFFFF);
+                }
+
+                var val32 = ptr.ToInt32();
+                return (short)(val32 & 0xFFFF);
+            }
+        }
+    }
+}
--- YuzuMarker/YuzuMarker/View/MainWindow.xaml.cs
+++ YuzuMarker/YuzuMarker/View/MainWindow.xaml.cs
@@ -56,48 +56,13 @@ namespace YuzuMarker.View
             switch (msg)
             {
                 case WM_MOUSEHWHEEL:
-                    int tilt = HIWORD(wParam);
+                    var tilt = BitOperation.HIWORD(wParam);
                     OnMouseTilt(tilt);
                     return (IntPtr) 1;
             }
             return IntPtr.Zero;
         }
 
-        /// <summary>
-        /// 取指针所在高位数值。
-        /// </summary>
-        private static int HIWORD(IntPtr ptr)
-        {
-            unchecked
-            {
-                if (Environment.Is64BitOperatingSystem)
-                {
-                    var val64 = ptr.ToInt64();
-                    return (short) ((val64 >> 16) & 0xFFFF);
-                }
-                var val32 = ptr.ToInt32();
-                return (short) ((val32 >> 16) & 0xFFFF);   
-            }
-        }
-
-        /// <summary>
-        /// 取指针所在低位数值。
-        /// </summary>
-        private static int LOWORD(IntPtr ptr)
-        {
-            unchecked
-            {
-                if (Environment.Is64BitOperatingSystem)
-                {
-                    var val64 = ptr.ToInt64();
-                    return (short)(val64 & 0xFFFF);
-                }
-
-                var val32 = ptr.ToInt32();
-                return (short)(val32 & 0xFFFF);
-            }
-        }
-        
         private void OnMouseTilt(double tilt)
         {
             if (!(Mouse.DirectlyOver is UIElement element)) return;
