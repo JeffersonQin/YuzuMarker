01e9c10b9693fe578db30bc24d1ef24ffba92487
gyro永不抽风 <1247006353@qq.com>
2021-11-22 22:29:34 +0800

    feat: add notation base class

---
+++ YuzuMarker/YuzuMarker.BasicDataFormat/BasicYuzuNotation.cs
@@ -0,0 +1,20 @@
+﻿using YuzuMarker.Common;
+
+namespace YuzuMarker.BasicDataFormat
+{
+    public class BasicYuzuNotation : NotifyObject
+    {
+        private BasicYuzuNotationGroup _parentNotationGroup;
+
+        public BasicYuzuNotationGroup ParentNotationGroup
+        {
+            get => _parentNotationGroup;
+            set => SetProperty(ref _parentNotationGroup, value);
+        }
+
+        public BasicYuzuNotation(BasicYuzuNotationGroup parentNotationGroup)
+        {
+            ParentNotationGroup = parentNotationGroup;
+        }
+    }
+}
