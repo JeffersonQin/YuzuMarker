43c9d947c8ebf2e2c0b1b644289e239959c93017
gyro永不抽风 <1247006353@qq.com>
2021-11-25 23:50:27 +0800

    feat: add color cleaning notation

---
+++ YuzuMarker/YuzuMarker.DataFormat/YuzuColorCleaningNotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using YuzuMarker.Common;
+
+namespace YuzuMarker.DataFormat
+{
+    public class YuzuColorCleaningNotation : YuzuCleaningNotation
+    {
+        public YuzuColorCleaningNotation(YuzuNotationGroup parentNotationGroup) : 
+            base(parentNotationGroup, YuzuCleaningNotationType.Color) {}
+        
+        public YuzuColorCleaningNotation(YuzuNotationGroup parentNotationGroup, Color color) : 
+            base(parentNotationGroup, YuzuCleaningNotationType.Color)
+        {
+            CleaningNotationColor = color;
+        }
+
+        private Color _cleaningNotationColor;
+
+        public Color CleaningNotationColor
+        {
+            get => _cleaningNotationColor;
+            set => UndoRedoManager.PushAndPerformRecord(o =>
+            {
+                var nowValue = CleaningNotationColor;
+                SetProperty(ref _cleaningNotationColor, (Color)o);
+                return nowValue;
+            }, o =>
+            {
+                var nowValue = CleaningNotationColor;
+                o ??= value;
+                SetProperty(ref _cleaningNotationColor, (Color)o);
+                return nowValue;
+            });
+        }
+    }
+}
