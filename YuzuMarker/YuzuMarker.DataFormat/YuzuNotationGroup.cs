0371ac567fc7cf156e41a231c6fe81f2becb66a7
gyro永不抽风 <1247006353@qq.com>
2021-11-25 23:51:48 +0800

    refactor: redesign notation group

--- YuzuMarker/YuzuMarker.DataFormat/YuzuNotationGroup.cs
+++ YuzuMarker/YuzuMarker.DataFormat/YuzuNotationGroup.cs
@@ -4,70 +4,61 @@ using System.IO;
 using System.Text;
 using OpenCvSharp;
 using YuzuMarker.BasicDataFormat;
+using YuzuMarker.Common;
 
 namespace YuzuMarker.DataFormat
 {
     public class YuzuNotationGroup : BasicYuzuNotationGroup
     {
-        // TODO: refactor: 删掉这个 ResourcesTracker
-        public ResourcesTracker NotationResourcesTracker;
-        
-        public YuzuCleaningNotation CleaningNotation { get; set; }
+        private YuzuCleaningNotation _cleaningNotation;
 
+        public YuzuCleaningNotation CleaningNotation
+        {
+            get => _cleaningNotation;
+            set => UndoRedoManager.PushAndPerformRecord(o =>
+            {
+                var nowValue = CleaningNotation;
+                SetProperty(ref _cleaningNotation, (YuzuCleaningNotation)o);
+                return nowValue;
+            }, o =>
+            {
+                var nowValue = CleaningNotation;
+                o ??= value;
+                SetProperty(ref _cleaningNotation, (YuzuCleaningNotation)o);
+                return nowValue;
+            }, o => ((YuzuCleaningNotation)o).Dispose());
+        }
+        
+        // Other kinds of notations
+        
         public YuzuNotationGroup(YuzuImage parentImage, int x, int y, string text, bool finished) : base(parentImage, x, y, text, finished)
         {
-            CleaningNotation = new YuzuCleaningNotation(YuzuCleaningNotationType.Normal);
+            CleaningNotation = new YuzuColorCleaningNotation(this);
         }
 
         public YuzuNotationGroup(YuzuImage parentImage, long timestamp, int x, int y, string text, bool finished) : base(parentImage, timestamp, x, y, text, finished)
         {
-            CleaningNotation = new YuzuCleaningNotation(YuzuCleaningNotationType.Normal);
+            CleaningNotation = new YuzuColorCleaningNotation(this);
         }
         
-        // TODO: refactor: 把这个方法的实现放到 YuzuCleaningNotation (和各个子类) 中
-        public void LoadNotationResource()
+        public void Load()
         {
-            NotationResourcesTracker = new ResourcesTracker();
-            var tempMaskImagePath = Path.Combine(ParentImage.GetImageTempPath(), "./" + Timestamp + "-cleaning-mask.png");
-            if (!File.Exists(tempMaskImagePath))
-            {
-                var maskImagePath = Path.Combine(ParentImage.GetImageNotationPath(), "./" + Timestamp + "-cleaning-mask.png");
-                if (!File.Exists(maskImagePath))
-                {
-                    using var src = new Mat(ParentImage.GetImageFilePath());
-                    CleaningNotation.CleaningMask = NotationResourcesTracker.T<UMat>(new UMat(new Size(src.Cols, src.Rows),
-                        MatType.CV_8UC1, new Scalar(0)));
-                }
-                else
-                {
-                    CleaningNotation.CleaningMask = NotationResourcesTracker.T<UMat>(new UMat());
-                    using var src = Cv2.ImRead(maskImagePath, ImreadModes.Grayscale);
-                    src.CopyTo(CleaningNotation.CleaningMask);
-                }
-            }
-            else
-            {
-                CleaningNotation.CleaningMask = NotationResourcesTracker.T<UMat>(new UMat());
-                using var src = Cv2.ImRead(tempMaskImagePath, ImreadModes.Grayscale);
-                src.CopyTo(CleaningNotation.CleaningMask);
-            }
+            CleaningNotation.Load();
         }
 
-        public void WriteNotationResource()
+        public void Write()
         {
-            var tempMaskImagePath = Path.Combine(ParentImage.GetImageTempPath(), "./" + Timestamp + "-cleaning-mask.png");
-            var writeMat = CleaningNotation.CleaningMask.GetMat(AccessFlag.READ);
-            Cv2.ImWrite(tempMaskImagePath, writeMat);
-            writeMat.Dispose();
+            CleaningNotation.Write();
         }
 
-        public void UnloadNotationResource()
+        public void Dispose()
         {
-            WriteNotationResource();
-            CleaningNotation.CleaningMask.Dispose();
-            NotationResourcesTracker.Dispose();
+            CleaningNotation.Dispose();
         }
 
-        // Other kinds of notations
+        public void Unload()
+        {
+            CleaningNotation.Unload();
+        }
     }
 }
