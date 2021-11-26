7900d2c97e26a6f5cf9dff9c04a54732a31aab34
gyro永不抽风 <1247006353@qq.com>
2021-11-26 12:05:11 +0800

    feat: add a more concise SetProperty implementation integrated with undo redo management

--- YuzuMarker/YuzuMarker.Common/NotifyObject.cs
+++ YuzuMarker/YuzuMarker.Common/NotifyObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -19,6 +20,51 @@ namespace YuzuMarker.Common
             backingStore = value;
             onChanged?.Invoke();
             RaisePropertyChanged(propertyName);
+            return true;
+        }
+
+        public bool SetProperty<T>(T value, [CallerMemberName] string propertyName = "",
+            Action beforeChanged = null, Action onChanged = null)
+        {
+            var backingName = "_" + propertyName[0].ToString().ToLower() + propertyName.Substring(1);
+            Type type = null;
+            FieldInfo backingInstance = null;
+            do
+            {
+                type = type == null ? GetType() : type.BaseType;
+                if (type == null) return false;
+                backingInstance = type.GetField(backingName, BindingFlags.NonPublic | BindingFlags.Instance);
+            } while (backingInstance == null);
+            
+            if (EqualityComparer<T>.Default.Equals((T)backingInstance.GetValue(this), value))
+                return false;
+            
+            var attribute = GetType().GetProperty(propertyName)?.GetCustomAttribute<Undoable>();
+            if (attribute == null)
+            {
+                beforeChanged?.Invoke();
+                backingInstance.SetValue(this, value);
+                onChanged?.Invoke();
+                RaisePropertyChanged(propertyName);
+            }
+            else
+            {
+                UndoRedoManager.PushAndPerformRecord(attribute.UndoAction ?? (o =>
+                {
+                    var nowValue = backingInstance.GetValue(this);
+                    backingInstance.SetValue(this, o);
+                    RaisePropertyChanged(propertyName);
+                    return nowValue;
+                }), attribute.RedoAction ?? (o =>
+                {
+                    var nowValue = backingInstance.GetValue(this);
+                    o ??= value;
+                    backingInstance.SetValue(this, o);
+                    RaisePropertyChanged(propertyName);
+                    return nowValue;
+                }), attribute.DisposeAction);
+            }
+            
             return true;
         }
 
