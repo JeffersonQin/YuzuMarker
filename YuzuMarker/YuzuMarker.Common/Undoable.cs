9cad7ee36cfced0f8010fba238ae443c07f0b82b
gyro永不抽风 <1247006353@qq.com>
2021-11-27 15:05:48 +0800

    fix: remove fields in undoable attribute

--- YuzuMarker/YuzuMarker.Common/Undoable.cs
+++ YuzuMarker/YuzuMarker.Common/Undoable.cs
@@ -3,25 +3,5 @@
 namespace YuzuMarker.Common
 {
     [AttributeUsage(AttributeTargets.Property)]
-    public class Undoable : Attribute
-    {
-        public UndoRedoRecord.DelegateActionWithValue DisposeAction;
-
-        public UndoRedoRecord.DelegateActionWithAndReturnValue UndoAction, RedoAction;
-
-        public Undoable()
-        {
-            
-        }
-        
-        public Undoable(UndoRedoRecord.DelegateActionWithAndReturnValue undoAction = null,
-            UndoRedoRecord.DelegateActionWithAndReturnValue redoAction = null,
-            UndoRedoRecord.DelegateActionWithValue disposeAction = null)
-
-        {
-            UndoAction = undoAction;
-            RedoAction = redoAction;
-            DisposeAction = disposeAction;
-        }
-    }
+    public class Undoable : Attribute {}
 }
