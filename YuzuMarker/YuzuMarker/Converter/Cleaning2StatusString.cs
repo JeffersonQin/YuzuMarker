cbf186062c0eefb57c09d627928ea5444bdebb4c
gyro永不抽风 <1247006353@qq.com>
2021-11-27 15:11:50 +0800

    feat: redesign cleaning status string

--- YuzuMarker/YuzuMarker/Converter/Cleaning2StatusString.cs
+++ YuzuMarker/YuzuMarker/Converter/Cleaning2StatusString.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Drawing;
 using System.Globalization;
 using System.Windows.Data;
 using YuzuMarker.DataFormat;
+using YuzuMarker.Utils;
 
 namespace YuzuMarker.Converter
 {
@@ -10,9 +12,24 @@ namespace YuzuMarker.Converter
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             YuzuCleaningNotation cleaningNotation = (YuzuCleaningNotation) value;
-            if (cleaningNotation.CleaningNotationType == YuzuCleaningNotationType.Custom) return "PS自定义蒙版";
-            if (cleaningNotation.IsEmpty()) return "不涂白";
-            return "区域已选择";
+            
+            var statusString = cleaningNotation.CleaningMask.IsEmpty() ? "[未选区]" : "[已选区]";
+
+            switch (cleaningNotation.CleaningNotationType)
+            {
+                case YuzuCleaningNotationType.Color:
+                    if (((YuzuColorCleaningNotation)cleaningNotation).CleaningNotationColor == Color.Transparent)
+                        statusString += "[未选色]";
+                    else statusString += "[已选色]";
+                    break;
+                case YuzuCleaningNotationType.Impainting:
+                    if (((YuzuImpaintingCleaningNotation)cleaningNotation).ImpaintingImage.IsEmpty())
+                        statusString += "[未修复]";
+                    else statusString += "[已修复]";
+                    break;
+            }
+
+            return statusString;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
