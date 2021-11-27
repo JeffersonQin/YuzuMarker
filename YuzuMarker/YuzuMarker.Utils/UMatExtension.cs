5beae09d678365db3ccc597dc3c74530738b3c47
gyro永不抽风 <1247006353@qq.com>
2021-11-27 15:05:04 +0800

    refactor: break down UMatExtension into wpf part and non-wpf part

---
+++ YuzuMarker/YuzuMarker.Utils/UMatExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace YuzuMarker.Utils
+{
+    public static partial class UMatExtension
+    {
+        public static bool IsEmpty(this UMat umat)
+        {
+            if (umat == null) return true;
+            if (umat.IsDisposed) return true;
+            if (umat.CvPtr != IntPtr.Zero)
+                return Cv2.CountNonZero(umat) == 0;
+            return true;
+        }
+
+        public static bool SafeDispose(this UMat umat)
+        {
+            if (umat == null) return false;
+            if (umat.IsDisposed) return false;
+            if (umat.CvPtr == IntPtr.Zero) return false;
+            umat.Dispose();
+            return true;
+        }
+
+        public static UMat SafeClone(this UMat umat)
+        {
+            if (umat == null) return null;
+            if (umat.IsDisposed) return null;
+            if (umat.CvPtr == IntPtr.Zero) return null;
+            return umat.Clone();
+        }
+    }
+}
--- YuzuMarker/YuzuMarker/Utils/UMatExtension.cs
+++ YuzuMarker/YuzuMarker/Utils/UMatExtension.cs
@@ -5,7 +5,7 @@ using OpenCvSharp.WpfExtensions;
 
 namespace YuzuMarker.Utils
 {
-    public static class UMatExtension
+    public static partial class UMatExtension
     {
         public static WriteableBitmap To4ChannelImage(this UMat umat, Scalar color)
         {
@@ -34,32 +34,6 @@ namespace YuzuMarker.Utils
             dstMask.Dispose();
             c1Mask.Dispose();
             return image;
-        }
-
-        public static bool IsEmpty(this UMat umat)
-        {
-            if (umat == null) return true;
-            if (umat.IsDisposed) return true;
-            if (umat.CvPtr != IntPtr.Zero)
-                return Cv2.CountNonZero(umat) == 0;
-            return true;
-        }
-
-        public static bool SafeDispose(this UMat umat)
-        {
-            if (umat == null) return false;
-            if (umat.IsDisposed) return false;
-            if (umat.CvPtr == IntPtr.Zero) return false;
-            umat.Dispose();
-            return true;
-        }
-
-        public static UMat SafeClone(this UMat umat)
-        {
-            if (umat == null) return null;
-            if (umat.IsDisposed) return null;
-            if (umat.CvPtr == IntPtr.Zero) return null;
-            return umat.Clone();
         }
     }
 }
