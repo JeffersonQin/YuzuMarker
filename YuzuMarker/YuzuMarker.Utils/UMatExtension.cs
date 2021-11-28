dfc4d7bcd6d0875caf3cedaef4b205bcdc83309a
gyro永不抽风 <1247006353@qq.com>
2021-11-28 23:20:06 +0800

    refactor: renaming & safe method adaption in umat extension

--- YuzuMarker/YuzuMarker.Utils/UMatExtension.cs
+++ YuzuMarker/YuzuMarker.Utils/UMatExtension.cs
@@ -5,35 +5,35 @@ namespace YuzuMarker.Utils
 {
     public static partial class UMatExtension
     {
-        public static bool IsEmpty(this UMat umat)
+        public static bool IsEmpty(this UMat src)
         {
-            if (umat == null) return true;
-            if (umat.IsDisposed) return true;
-            if (umat.CvPtr == IntPtr.Zero) return true;
-            if (umat.Channels() == 1)
-                return Cv2.CountNonZero(umat) == 0;
+            if (src == null) return true;
+            if (src.IsDisposed) return true;
+            if (src.CvPtr == IntPtr.Zero) return true;
+            if (src.Channels() == 1)
+                return Cv2.CountNonZero(src) == 0;
             UMat singleChannelMat = new UMat();
-            Cv2.CvtColor(umat, singleChannelMat, ColorConversionCodes.BGR2GRAY);
+            Cv2.CvtColor(src, singleChannelMat, ColorConversionCodes.BGR2GRAY);
             bool isEmpty = Cv2.CountNonZero(singleChannelMat) == 0;
-            singleChannelMat.Dispose();
+            singleChannelMat.SafeDispose();
             return isEmpty;
         }
 
-        public static bool SafeDispose(this UMat umat)
+        public static bool SafeDispose(this UMat src)
         {
-            if (umat == null) return false;
-            if (umat.IsDisposed) return false;
-            if (umat.CvPtr == IntPtr.Zero) return false;
-            umat.Dispose();
+            if (src == null) return false;
+            if (src.IsDisposed) return false;
+            if (src.CvPtr == IntPtr.Zero) return false;
+            src.Dispose();
             return true;
         }
 
-        public static UMat SafeClone(this UMat umat)
+        public static UMat SafeClone(this UMat src)
         {
-            if (umat == null) return null;
-            if (umat.IsDisposed) return null;
-            if (umat.CvPtr == IntPtr.Zero) return null;
-            return umat.Clone();
+            if (src == null) return null;
+            if (src.IsDisposed) return null;
+            if (src.CvPtr == IntPtr.Zero) return null;
+            return src.Clone();
         }
     }
 }
