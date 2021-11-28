33e5d32673504c784a33e51a34d167d6a6bd8cfc
gyro永不抽风 <1247006353@qq.com>
2021-11-28 12:41:38 +0800

    feat: add AncestorNotifiableMarker attribute

---
+++ YuzuMarker/YuzuMarker.Common/AncestorNotifiableMarker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace YuzuMarker.Common
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class AncestorNotifiableMarker : Attribute
+    {
+        public string AncestorName { get; set; }
+
+        public AncestorNotifiableMarker(string ancestorName)
+        {
+            AncestorName = ancestorName;
+        }
+    }
+}
