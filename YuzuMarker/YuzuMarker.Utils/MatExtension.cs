aa82d369156eb821c992e2807f9adca74a439aa1
gyro永不抽风 <1247006353@qq.com>
2021-11-28 23:20:38 +0800

    feat: add mat extension

---
+++ YuzuMarker/YuzuMarker.Utils/MatExtension.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenCvSharp;
+
+namespace YuzuMarker.Utils
+{
+    public static class MatExtension
+    {
+        public static bool IsEmpty(this Mat src)
+        {
+            if (src == null) return true;
+            if (src.IsDisposed) return true;
+            if (src.CvPtr == IntPtr.Zero) return true;
+            if (src.Channels() == 1)
+                return Cv2.CountNonZero(src) == 0;
+            Mat singleChannelMat = new Mat();
+            Cv2.CvtColor(src, singleChannelMat, ColorConversionCodes.BGR2GRAY);
+            bool isEmpty = Cv2.CountNonZero(singleChannelMat) == 0;
+            singleChannelMat.SafeDispose();
+            return isEmpty;
+        }
+
+        public static bool SafeDispose(this Mat src)
+        {
+            if (src == null) return false;
+            if (src.IsDisposed) return false;
+            if (src.CvPtr == IntPtr.Zero) return false;
+            src.Dispose();
+            return true;
+        }
+
+        public static Mat SafeClone(this Mat src)
+        {
+            if (src == null) return null;
+            if (src.IsDisposed) return null;
+            if (src.CvPtr == IntPtr.Zero) return null;
+            return src.Clone();
+        }
+    }
+}
