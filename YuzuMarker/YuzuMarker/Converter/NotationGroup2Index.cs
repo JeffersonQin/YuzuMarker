9a8eaa1e5b0bddbd111b6a33251018c2fecb5b49
gyro永不抽风 <1247006353@qq.com>
2021-11-28 23:22:42 +0800

    feat: add NotationGroup2IndexString

--- YuzuMarker/YuzuMarker/Converter/NotationGroup2Index.cs
+++ YuzuMarker/YuzuMarker/Converter/NotationGroup2Index.cs
@@ -20,4 +20,18 @@ namespace YuzuMarker.Converter
             throw new NotImplementedException();
         }
     }
+    
+    public class NotationGroup2IndexString : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var notationGroup = (YuzuNotationGroup)values[0];
+            return notationGroup?.ParentImage.NotationGroups.IndexOf(notationGroup) + 1 + "";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
