313080746ae78b208f58b1fb83f250e554ac636d
gyro永不抽风 <1247006353@qq.com>
2021-11-28 18:42:26 +0800

    feat: update notationgroup -> index converter to be notifiable via dummy

--- YuzuMarker/YuzuMarker/Converter/NotationGroup2Index.cs
+++ YuzuMarker/YuzuMarker/Converter/NotationGroup2Index.cs
@@ -7,15 +7,15 @@ using YuzuMarker.DataFormat;
 
 namespace YuzuMarker.Converter
 {
-    public class NotationGroup2Index : IValueConverter
+    public class NotationGroup2Index : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var notationGroup = (YuzuNotationGroup)value;
+            var notationGroup = (YuzuNotationGroup)values[0];
             return notationGroup?.ParentImage.NotationGroups.IndexOf(notationGroup) + 1;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
