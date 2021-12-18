82a34ac984d60a49826227bcaf95d4d69eaf992d
gyro永不抽风 <1247006353@qq.com>
2021-12-18 22:52:20 +0800

    feat: add ValueWithDummyValueConverter

---
+++ YuzuMarker/YuzuMarker/Converter/ValueWithDummyValueConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace YuzuMarker.Converter
+{
+    public class ValueWithDummyValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values[0];
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
